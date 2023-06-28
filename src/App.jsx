7f7d0078d6669c0919c5f377b213ca1e12bcbbe3
afisman <afisman@gmail.com>
2023-06-28 12:01:07 +0200

    worked on navbar, still need to deal with some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,58 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import "./styles/Home.css";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
 
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+        <div className="container">
+          <main className="main">
+            <h1 className="title">
+              Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+            </h1>
 
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
+            <p className="description">
+              Get started by configuring your desired network in{" "}
+              <code className="code">src/index.js</code>, then modify the{" "}
+              <code className="code">src/App.js</code> file!
+            </p>
+
+            <div className="connect">
+              <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center' }} />
+            </div>
+
+            <div className="grid">
+              <a href="https://portal.thirdweb.com/" className="card">
+                <h2>Portal &rarr;</h2>
+                <p>
+                  Guides, references and resources that will help you build with
+                  thirdweb.
+                </p>
+              </a>
+
+              <a href="https://thirdweb.com/dashboard" className="card">
+                <h2>Dashboard &rarr;</h2>
+                <p>
+                  Deploy, configure and manage your smart contracts from the
+                  dashboard.
+                </p>
+              </a>
+
+              <a href="https://portal.thirdweb.com/templates" className="card">
+                <h2>Templates &rarr;</h2>
+                <p>
+                  Discover and clone template projects showcasing thirdweb features.
+                </p>
+              </a>
+            </div>
+          </main>
         </div>
+      </div>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
     </div>
+
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,34 @@ import CustomButton from './CustomButton';
 import { useStateContext } from '../context';
 
 const Navbar = () => {
-    const navigate = usenavigate();
+    const navigate = useNavigate();
     const [isActive, setIsActive] = useState('dashboard');
 
     const [toggleDrawer, setToggleDrawer] = useState(false);
 
-    const { connect, address } = useStateContext();
+    const { connect, address, disconnect } = useStateContext();
 
     return (
-        <div>Navbar</div>
+        <div className='flex md:flex-row flex-col-reverse 
+        justify-between mb-[35px] gap-6'>
+            <div className='lg:flex-1 flex flex-row max-w-[458px]
+            py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]'>
+
+            </div>
+
+            <div className='sm:flex hidden flex-row justify-end gap-4'>
+                <CustomButton
+                    btnType='button'
+                    title={address ? 'Download transactions' : 'Connect'}
+                    styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+                    handleClick={() => {
+                        address ? disconnect() : connect()
+                    }}
+                />
+
+            </div>
+
+        </div>
     )
 }
 
